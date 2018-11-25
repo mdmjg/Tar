8246e53a0496699ec35897f320d3b4ab69640d4d
wl1390 <wl1390@nyu.edu>
2018-11-25 18:00:05 +0400

    new

--- tsh.c
+++ tsh.c
@@ -171,7 +171,7 @@ void eval(char *cmdline)
 
     int bg = parseline(cmdline, argv);
     if (!builtin_cmd(argv)){
-        if ((pid = Fork()) == 0){
+        if ((pid = fork()) == 0){
             if (execve(argv[0],argv,environ) < 0){
                 printf("%s: Command not found. \n", argv[0]);
                 exit(0);
