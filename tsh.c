830f65b62a36a1b5fc36301574e32fc02da52165
wl1390 <october_li@yahoo.com>
2018-11-25 15:44:26 +0400

    name

--- tsh.c
+++ tsh.c
@@ -1,7 +1,7 @@
 /* 
  * tsh - A tiny shell program with job control
  * 
- * <Put your name and login ID here>
+ * Maria Jaramillo mdj308 - Li Wendi wl1390
  */
 #include <stdio.h>
 #include <stdlib.h>
