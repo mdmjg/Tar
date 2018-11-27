0127b169723636ec6428520d55b23bbe1a0345eb
wl1390 <wl1390@nyu.edu>
2018-11-27 22:57:22 +0400

    new

--- tsh.c
+++ tsh.c
@@ -297,7 +297,7 @@ void do_bgfg(char **argv)
 
     if ((argv[1][0] == '%')){
         if  (argv[1][1] == NULL || !isdigit(argv[1][1]) || (!(current_job = getjobjid(jobs, atoi(&argv[1][1]))))){
-            printf("%%[%s]: No such job\n",argv[1][1]);
+            printf("%%[%d]: No such job\n", atoi(argv[1][1]));
             return;
         }
         
